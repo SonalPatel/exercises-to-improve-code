50505d33568f5dbb47427f6487fea3b8127d2a67
Sonal Patel <sonal@justgiving.com>
2018-05-18 18:06:42 +0100

    function to check if value if greater than 10

--- functions/greater-than-ten/greater.js
+++ functions/greater-than-ten/greater.js
@@ -1,4 +1,4 @@
-function checkValue(highNumber) {
+/*function checkValue(highNumber) {
   if (highNumber > 10) {
     return true;
   } else {
@@ -6,5 +6,18 @@ function checkValue(highNumber) {
   }
 }
 highNumber = 12;
-//console.log(highNumber);
+console.log(highNumber);
 alert(highNumber);
+*/
+
+checkValue = x => {
+  if (x > 10) {
+    return "15 is greater than 10";
+    //executes if true
+  } else {
+    return "15 is lower than 10";
+    //executes if false
+  }
+};
+console.log(checkValue(15));
+alert(checkValue(15));
