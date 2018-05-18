d4308e1ba614a43900c6b993fe5c00f441199254
Sonal Patel <sonal@justgiving.com>
2018-05-18 15:10:00 +0100

    changed my function to use a fat arrow function.

--- functions/add/add.js
+++ functions/add/add.js
@@ -1,12 +1,2 @@
-//create a function to add two numbers together and output the results
-
-// Declare the function and pa
-function add(num1, num2) {
-  return num1 + num2;
-}
-//declare the variable and give it a value
-const result = add(5, 5);
-
-//add the numbers and print to console and dispaly in an alertbox
-console.log(result); //10
-alert(result);
+const add = (num1, num2) => num1 + num2;
+alert(add(2, 5));
