270780b3e3c29554121212473d58e64059b32d52
Sonal Patel <sonal@justgiving.com>
2018-05-18 12:55:21 +0100

    add function and creating an alert with the result

--- functions/add/add.js
+++ functions/add/add.js
@@ -1,13 +1,12 @@
 //create a function to add two numbers together and output the results
-//const addFunction = (5, 2)  =>  {return 5 + 2;}
-//console.log(addFunction);
 
-// Declare the function
-function add(x) {
-  return x + 5;
+// Declare the function and pa
+function add(num1, num2) {
+  return num1 + num2;
 }
 //declare the variable and give it a value
-const result = add(10);
+const result = add(5, 5);
 
-//add the numbers and print to console
-console.log(result);
+//add the numbers and print to console and dispaly in an alertbox
+console.log(result); //10
+alert(result);
