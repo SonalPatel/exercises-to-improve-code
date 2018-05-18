76966561586920f89de9af2fe7d0b2dab483068a
Sonal Patel <sonal@justgiving.com>
2018-05-18 12:32:34 +0100

    adding 2 numbers and outputting int he console

---
+++ functions/add/add.js
@@ -0,0 +1,13 @@
+//create a function to add two numbers together and output the results
+//const addFunction = (5, 2)  =>  {return 5 + 2;}
+//console.log(addFunction);
+
+// Declare the function
+function add(x) {
+  return x + 5;
+}
+//declare the variable and give it a value
+const result = add(10);
+
+//add the numbers and print to console
+console.log(result);
