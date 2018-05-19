6a17ca23fedafefec06393de6260613c190ff3ba
Sonal Patel <sonal@justgiving.com>
2018-05-19 14:55:20 +0100

    first commit of comparing strings - step one find the length of each string

---
+++ functions/longest-word/long.js
@@ -0,0 +1,6 @@
+//given two strings return the one with the longest word
+const nameSonal = "sonal";
+const NamePete = "pete";
+
+document.write("nameSonal.length is: " + nameSonal.length);
+document.write("NamePete.length is: " + NamePete.length);
