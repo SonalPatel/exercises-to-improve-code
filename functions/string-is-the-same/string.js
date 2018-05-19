1c62a4195b0326ad29e290e793177302928b5c5a
Sonal Patel <sonal@justgiving.com>
2018-05-19 14:38:43 +0100

    comparing two strings and alerting a true or false

--- functions/string-is-the-same/string.js
+++ functions/string-is-the-same/string.js
@@ -1,27 +1,9 @@
 //check if one string equals another.
 
-/*const test = "123";
-if (test === "123") {
-  alert(true);
-} else {
-  alert(false);
-}
-alert(test(1323));
-
-
-function testString("123"); {
-    if("123") {
-        alert(true);
-    } else {
-        alert(false);
-    }
-}
-alert(testString("13232))
-*/
-
 const s = "123";
+const p = "321";
 
-if (s === "123") {
+if (s === p) {
   alert(true);
 } else {
   alert(false);
