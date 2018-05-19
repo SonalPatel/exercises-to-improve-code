4bb124f8a694d153579c2681b7706158f47d1504
Sonal Patel <sonal@justgiving.com>
2018-05-19 14:36:40 +0100

    first commit of srting function

--- functions/string-is-the-same/string.js
+++ functions/string-is-the-same/string.js
@@ -1,9 +1,28 @@
 //check if one string equals another.
 
-const test = "123";
+/*const test = "123";
 if (test === "123") {
   alert(true);
 } else {
   alert(false);
 }
-alert(test(1223));
+alert(test(1323));
+
+
+function testString("123"); {
+    if("123") {
+        alert(true);
+    } else {
+        alert(false);
+    }
+}
+alert(testString("13232))
+*/
+
+const s = "123";
+
+if (s === "123") {
+  alert(true);
+} else {
+  alert(false);
+}
