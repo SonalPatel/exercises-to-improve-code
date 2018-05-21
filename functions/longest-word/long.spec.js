72d6cb9a45c7588c13724d2842eace314e17b3a2
Unknown <sonal.patel@justgiving.com>
2018-05-21 18:15:35 +0100

    trying to fix error in unit test for longest word

--- functions/longest-word/long.spec.js
+++ functions/longest-word/long.spec.js
@@ -1,6 +1,6 @@
-const long = require("./long.js");
+const findlongestword = require("./long.js");
 
-describe("findlongestword", () => {
+describe("findThelongestword", () => {
   it("should compare the length of two strings and return the longest one", () => {
     const result = findlongestword("lorem", "loremipsum");
     expect(result).toBe("loremipsum");
