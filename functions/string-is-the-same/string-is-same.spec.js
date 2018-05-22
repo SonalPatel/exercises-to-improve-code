f23fddd7e79d3eea51f3dcc6be13262e4859b0c8
Unknown <sonal.patel@justgiving.com>
2018-05-22 09:51:59 +0100

    fixes to unit tests

--- functions/string-is-the-same/string-is-same.spec.js
+++ functions/string-is-the-same/string-is-same.spec.js
@@ -5,4 +5,8 @@ describe("isTheSameWord", () => {
     const result = isSameWord("word1", "word2");
     expect(result).toBe(false);
   });
+  it("should check if one string is equal to another and result in true or false", () => {
+    const result = isSameWord("word1", "word1");
+    expect(result).toBe(true);
+  });
 });
